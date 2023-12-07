5297cef4a00edc039e201d735740b40d66271710
kodunaiff <kodunaiff@gmail.com>
2023-12-07 16:25:54 +0800

    used get_object_or_404

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,7 +1,5 @@
 import folium
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Pokemon, PokemonEntity
@@ -55,42 +53,36 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    try:
-        pokemon = Pokemon.objects.filter(id=pokemon_id)
-
-    except ObjectDoesNotExist:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
-
-    pokemon_id = pokemon[0]
+    pokemon = get_object_or_404(Pokemon, id=pokemon_id)
     pokemon_on_page = {
-        'pokemon_id': pokemon_id.id,
-        'img_url': pokemon_id.image.url,
-        'title_ru': pokemon_id.title,
-        'description': pokemon_id.description,
-        'title_en': pokemon_id.title_en,
-        'title_jp': pokemon_id.title_jp,
+        'pokemon_id': pokemon.id,
+        'img_url': pokemon.image.url,
+        'title_ru': pokemon.title,
+        'description': pokemon.description,
+        'title_en': pokemon.title_en,
+        'title_jp': pokemon.title_jp,
     }
 
-    if pokemon_id.previous_evolution:
+    if pokemon.previous_evolution:
         previous_evolution = {
-            "title_ru": pokemon_id.previous_evolution.title,
-            "pokemon_id": pokemon_id.previous_evolution.id,
-            "img_url": pokemon_id.previous_evolution.image.url,
+            "title_ru": pokemon.previous_evolution.title,
+            "pokemon_id": pokemon.previous_evolution.id,
+            "img_url": pokemon.previous_evolution.image.url,
         }
         pokemon_on_page['previous_evolution'] = previous_evolution
 
-    if pokemon_id.next_evolutions.first():
+    if pokemon.next_evolutions.first():
         next_evolution = {
-            "title_ru": pokemon_id.next_evolutions.first().title,
-            "pokemon_id": pokemon_id.next_evolutions.first().id,
-            "img_url": pokemon_id.next_evolutions.first().image.url
+            "title_ru": pokemon.next_evolutions.first().title,
+            "pokemon_id": pokemon.next_evolutions.first().id,
+            "img_url": pokemon.next_evolutions.first().image.url
         }
         pokemon_on_page['next_evolution'] = next_evolution
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     time_now = timezone.now()
     for pokemon_entity in PokemonEntity.objects.filter(
-            pokemon=pokemon_id, appeared_at__lt=time_now,
+            pokemon=pokemon, appeared_at__lt=time_now,
             disappeared_at__gt=time_now
     ):
         add_pokemon(
